a0201a6b74ff6d0402717bbfa902ce33f96f14f4
onatceltik <64609605+onatceltik@users.noreply.github.com>
2020-08-22 14:53:40 +0300

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection()) // Opens connection
+            using (var channel = connection.CreateModel()) // Opens channel
+            {
+                channel.QueueDeclare("BasicTestQueue", false, false, false, null); // Queue declaring
+
+                var consumer = new EventingBasicConsumer(channel);
+                
+                consumer.Received += (model, ea) =>
+                 {
+                     var body = ea.Body.Span;
+                     var message = Encoding.UTF8.GetString(body);
+                     Console.WriteLine("Received message: {0}", message);
+                 };
+
+                // Get the message from queue and send it to consumer
+                channel.BasicConsume("BasicTestQueue", true, consumer);
+            }
+
+            Console.WriteLine("Press [enter] to exit the Consumer App ...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            using (var connection = factory.CreateConnection()) // Opens connection
+            using (var channel = connection.CreateModel()) // Opens channel
+            {
+                channel.QueueDeclare("BasicTestQueue", false, false, false, null); // Queue declaring
+
+                // this is the message that we send to our created queue
+                string message = "Getting started with .Net Core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                // Publish message       v*** Queue ****v 
+                channel.BasicPublish("", "BasicTestQueue", null, body);
+                Console.WriteLine("Sent message {0}...", message);
+            }
+
+            Console.WriteLine("Press [enter] to exit the Sender App ...");
+            Console.ReadLine();
+        }
+    }
+}
